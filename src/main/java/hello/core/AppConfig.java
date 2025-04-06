31a75effdfac54f95fb06bf4a9b0cf25441f5490
azure1307 <azure1307@gmail.com>
2025-04-06 23:44:23 +0900

    AppConfig 수정2

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -13,13 +13,13 @@ import hello.core.order.OrderServiceImpl;
 public class AppConfig {
 
 	public MemberService memberService() {
-		return new MemberServiceImpl(new MemoryMemberRepository());
+		return new MemberServiceImpl(memberRepository());
 	}
 
 	public OrderService orderService() {
 		return new OrderServiceImpl(
-			new MemoryMemberRepository(),
-			new FixDiscountPolicy()
+			memberRepository(),
+			discountPolicy()
 		);
 	}
 
